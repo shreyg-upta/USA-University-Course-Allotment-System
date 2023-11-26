3eee08584bb26c8e74f14bdbdd0ef034072da317
Shrey Gupta <f20221121@goa.bits-pilani.ac.in>
2023-11-26 16:13:26 +0530

    inserted the map

--- main.cpp
+++ main.cpp
@@ -11,39 +11,90 @@ int main()
     vector <Professor> allProfs;
     // input aditya dekh lega 
 
-
+    map<Course, vector<int>> initialMap;
+    map<Course, vector<int>> finalMap;
     CourseList allFDCDCs; // Adding all the FD CDCs offered in a master CourseList
-    for (size_t i = 0; i < allProfs.size(); i++)
+    for (int i = 0; i < allProfs.size(); i++)
     {
-        for (size_t j = 0; j < allProfs[i].FDCDCPreferences.size(); j++){
+        for (int j = 0; j < allProfs[i].FDCDCPreferences.size(); j++){
         allFDCDCs.checkandadd(allProfs[i].FDCDCPreferences[j]);
         }
     }
 
     CourseList allFDElectives; // Adding all the FD Electives offered in a master CourseList
-    for (size_t i = 0; i < allProfs.size(); i++)
+    for (int i = 0; i < allProfs.size(); i++)
     {
-        for (size_t j = 0; j < allProfs[i].FDElectivePreferences.size(); j++){
+        for (int j = 0; j < allProfs[i].FDElectivePreferences.size(); j++){
         allFDElectives.checkandadd(allProfs[i].FDElectivePreferences[j]);
         }
     }
 
     CourseList allHDCDCs; // Adding all the HD CDCs offered in a master CourseList
-    for (size_t i = 0; i < allProfs.size(); i++)
+    for (int i = 0; i < allProfs.size(); i++)
     {
-        for (size_t j = 0; j < allProfs[i].HDCDCPreferences.size(); j++){
+        for (int j = 0; j < allProfs[i].HDCDCPreferences.size(); j++){
         allHDCDCs.checkandadd(allProfs[i].HDCDCPreferences[j]);
         }
     }
 
     CourseList allHDElectives; // Adding all the HD Electives offered in a master CourseList
-    for (size_t i = 0; i < allProfs.size(); i++)
+    for (int i = 0; i < allProfs.size(); i++)
     {
-        for (size_t j = 0; j < allProfs[i].HDElectivePreferences.size(); j++){
+        for (int j = 0; j < allProfs[i].HDElectivePreferences.size(); j++){
         allHDElectives.checkandadd(allProfs[i].HDElectivePreferences[j]);
         }
     }
      
+
+for(const auto& course : allFDCDCs.getList()) {
+    initialMap[course] = vector<int>(allProfs.size(), 0);
+    finalMap[course] = vector<int>(allProfs.size(), 0);
+}
+for(const auto& course : allHDCDCs.getList()) {
+    initialMap[course] = vector<int>(allProfs.size(), 0);
+    finalMap[course] = vector<int>(allProfs.size(), 0);
+}
+for(const auto& course : allFDElectives.getList()) {
+    initialMap[course] = vector<int>(allProfs.size(), 0);
+    finalMap[course] = vector<int>(allProfs.size(), 0);
+}
+for(const auto& course : allHDElectives.getList()) {
+    initialMap[course] = vector<int>(allProfs.size(), 0);
+    finalMap[course] = vector<int>(allProfs.size(), 0);
+}
+   
+    for (int i = 0; i < allProfs.size(); i++)
+    {
+        for (int j = 0; j < allProfs[i].FDCDCPreferences.size(); j++){
+        initialMap[allProfs[i].FDCDCPreferences[j]][i] = 2;
+        }
+    }
+
+   
+    for (int i = 0; i < allProfs.size(); i++)
+    {
+        for (int j = 0; j < allProfs[i].FDElectivePreferences.size(); j++){
+            initialMap[allProfs[i].FDElectivePreferences[j]][i] = 2;
+        }
+    }
+
+
+    for (int i = 0; i < allProfs.size(); i++)
+    {
+        for (int j = 0; j < allProfs[i].HDCDCPreferences.size(); j++){
+        initialMap[allProfs[i].HDCDCPreferences[j]][i] = 2;
+        }
+    }
+
+
+    for (int i = 0; i < allProfs.size(); i++)
+    {
+        for (int j = 0; j < allProfs[i].HDElectivePreferences.size(); j++){
+       initialMap[allProfs[i].HDElectivePreferences[j]][i] = 2;
+        }
+    }
+
     
+
   return 0;
 }
